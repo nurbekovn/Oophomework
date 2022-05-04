e16b319cadda48aec604ea378266667e80b23589
nurbekov <nur.nurbekov.01@gmail.com>
2022-05-04 20:13:10 +0600

    Homework11

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.a=1;
+        triangle.b=2;
+        triangle.c=3;
+        triangle.area(triangle.a, triangle.b, triangle.c);
+
+    }
+}
+
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+    void area(int a, int b, int c) {
+        System.out.println(a * b * c + "  = area of Triangle");
+    }
+}
